70be16d4ae1dc869c3bd200ab139be8158416daa
Esscorial <esscorial@yandex.ru>
2022-03-20 22:03:46 +0500

    testByIp1

--- src/test/java/GeoServiceImplTest.java
+++ src/test/java/GeoServiceImplTest.java
@@ -1,15 +1,21 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
 import ru.netology.geo.GeoServiceImpl;
 
 public class GeoServiceImplTest {
+
     @Test
-    void testByIp() {
+    void testByIp1() {
         GeoService geoService = new GeoServiceImpl();
-        Location act = geoService.byIp("127.56.0.3");
-        Location exp = new Location(null, null, null, 0);
-        Assertions.assertNotEquals(exp, act);
+
+        Location act1 = geoService.byIp("172.193.0.21");
+        Assertions.assertEquals(Country.RUSSIA, act1.getCountry());
+
+        Location act2 = geoService.byIp("96.193.0.21");
+        Assertions.assertEquals(Country.USA, act2.getCountry());
+
     }
 }
